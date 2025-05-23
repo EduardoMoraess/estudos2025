8be28f0c9c7b754976f29d43bcdd966e74537d70
EduardoMoraes <eduzinho2118@gmail.com>
2025-05-23 20:55:49 -0300

    b

--- aulas_js/ex001.js
+++ aulas_js/ex001.js
@@ -1,7 +1,30 @@
-const Btn = document.querySelectorAll('button');
+/*const Btn = document.querySelectorAll('button');
 
 Btn.addEventListener('click', ()=>{
     Btn.forEach(Button=>{
         alert(`Voce apertou no btn ${Button.innerHTML}`);
     })
-})
+})*/
+
+class Pessoa{
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    apresentar(){
+        console.log(`Ola meu nome é, ${this.nome} e tenho ${this.idade} idade`);
+    }
+    felizAniversario(){
+        this.idade +=1;
+        console.log(`Feliz aniversario ${this.nome} agora vc tem ${this.idade} anos`);
+    }
+}
+
+const pessoa1 = new Pessoa("Noinha", 18);
+const pessoa2 = new Pessoa("<DEV_DUDU>", 20);
+
+pessoa1.apresentar();
+pessoa1.felizAniversario();
+
+pessoa2.apresentar();
+pessoa2.felizAniversario();
